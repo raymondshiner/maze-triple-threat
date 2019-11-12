c73c57eba3e985fdb2c7c767fe76807ceea0b039
Spencer <Curley>
2019-11-11 17:53:09 -0800

    updated to show that the database is currently working

--- Maze/src/MultipleChoice.java
+++ Maze/src/MultipleChoice.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.sql.*;
 
 public class MultipleChoice extends Question{
@@ -6,14 +7,25 @@ public class MultipleChoice extends Question{
     public MultipleChoice() {
 
         super("the test of the question class" );
-        // this is a problem because it uses the absolute path and my path is different than others so need to fiqure out how to use relitive paths and make it cross platform or just move the database file into the defualt directory that jdbc will look for the database .db file 
+        String initPath;
+        File databasePath = null;
+        try{
+            initPath =  ".." + File.separator + ".." + File.separator + "test.db" ;
+
+            databasePath = new File(initPath);
+            System.out.println(initPath + "\n" + databasePath.getAbsolutePath());
+        }catch(Exception e ){
+            System.out.println(e);
+        }
+
+        // this is a problem because it uses the absolute path and my path is different than others so need to fiqure out how to use relitive paths and make it cross platform or just move the database file into the defualt directory that jdbc will look for the database .db file
         String URL = "jdbc:sqlite:/Users/spencercurley/Desktop/cscd350/maze-triple-threat/database/test.db";
         String USR = "root";
         String password ="thisisastupidpassword";
         Connection myCon = null;
         try{
             Class.forName("org.sqlite.JDBC");
-            myCon = DriverManager.getConnection(URL,USR ,password);
+            myCon = DriverManager.getConnection( URL,USR ,password);
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -22,13 +34,17 @@ public class MultipleChoice extends Question{
             Statement test = myCon.createStatement();
             ResultSet results = test.executeQuery(sql);
             while(results.next()){
+                // this demonstrates how to get stuff back from the database
+                // to add data to a question we will need to figure out how to get the number of records in the database for the random number generator
+
                 System.out.println(results.getString("Question") + " and the answer is " + results.getString("Answer") );
+
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        // this will be replaced with the database information soon g
         this.thePossibleAnswers = new String[4];
         this.thePossibleAnswers[0] = "1";
         this.thePossibleAnswers[1] = "2";
