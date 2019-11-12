3c807cf256b55144f7598a7f8970fe1c9ad9a991
Spencer <Curley>
2019-11-12 13:08:21 -0800

    database access should be cross platform now

--- Maze/src/MultipleChoice.java
+++ Maze/src/MultipleChoice.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 
 public class MultipleChoice extends Question{
@@ -10,23 +11,23 @@ public class MultipleChoice extends Question{
         String initPath;
         File databasePath = null;
         try{
-            initPath =  ".." + File.separator + ".." + File.separator + "test.db" ;
+            initPath =  ".." +File.separator +  "database" + File.separator+"test.db" ;
 
             databasePath = new File(initPath);
-            System.out.println(initPath + "\n" + databasePath.getAbsolutePath());
+           // System.out.println(initPath + "\n" + databasePath.getCanonicalPath());
         }catch(Exception e ){
             System.out.println(e);
         }
 
-        // this is a problem because it uses the absolute path and my path is different than others so need to fiqure out how to use relitive paths and make it cross platform or just move the database file into the defualt directory that jdbc will look for the database .db file
+        // this is a problem because it uses the absolute path and my path is different than others so need to figure out how to use relitive paths and make it cross platform or just move the database file into the defualt directory that jdbc will look for the database .db file
         String URL = "jdbc:sqlite:/Users/spencercurley/Desktop/cscd350/maze-triple-threat/database/test.db";
         String USR = "root";
         String password ="thisisastupidpassword";
         Connection myCon = null;
         try{
             Class.forName("org.sqlite.JDBC");
-            myCon = DriverManager.getConnection( URL,USR ,password);
-        } catch (SQLException | ClassNotFoundException e) {
+            myCon = DriverManager.getConnection("jdbc:sqlite:" +databasePath.getCanonicalPath(),USR ,password);
+        } catch (SQLException | ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
         String sql = "SELECT * FROM Question";
