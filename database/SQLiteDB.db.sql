39b07245c2a6584864b40884bb744588753af3b8
dhoxie <dhoxie@eagles.ewu.edu>
2019-12-06 14:46:20 -0800

    finished adding questions

--- database/SQLiteDB.db.sql
+++ database/SQLiteDB.db.sql
@@ -1,7 +1,11 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "TrueFalse" (
-	"field1"	TEXT,
-	"field2"	TEXT
+	"Question"	TEXT,
+	"Answer"	TEXT
+);
+CREATE TABLE IF NOT EXISTS "ShortAnswer" (
+	"Question"	TEXT,
+	"Answer"	TEXT
 );
 CREATE TABLE IF NOT EXISTS "MultipleChoice" (
 	"Question"	TEXT,
@@ -11,16 +15,76 @@ CREATE TABLE IF NOT EXISTS "MultipleChoice" (
 	"c"	TEXT,
 	"d"	TEXT
 );
-CREATE TABLE IF NOT EXISTS "ShortAnswer" (
-	"Question"	TEXT,
-	"Answer"	TEXT
-);
-INSERT INTO "TrueFalse" VALUES ('Question','Answer');
 INSERT INTO "TrueFalse" VALUES ('Albus Dumbledore''s full name is Albus Percival Wulfric Brian Dumbledore','TRUE');
 INSERT INTO "TrueFalse" VALUES ('When the Death Eaters take over the Ministry of Magic, Yaxley''s title Head of the Department of International Magical Cooperation','FALSE');
 INSERT INTO "TrueFalse" VALUES ('Mad-Eye Moody is killed during the escape from Privet Drive','TRUE');
 INSERT INTO "TrueFalse" VALUES ('Harry''s second son is named Albus Sirius Potter','FALSE');
-INSERT INTO "MultipleChoice" VALUES ('Question','Answer','a','b','c','d');
+INSERT INTO "TrueFalse" VALUES ('Sirius called Severus Snape the nickname Snivellus during their years at Hogwarts','TRUE');
+INSERT INTO "TrueFalse" VALUES ('Harry gets expelled by the Ministry of Magic after the Dementor attack in the underpass because of his use of underage sorcery','TRUE');
+INSERT INTO "TrueFalse" VALUES ('Cornelius Fudge becomes the Minister for Magic after Rufus Scrimgeour''s death','FALSE');
+INSERT INTO "TrueFalse" VALUES ('Madame Maxime is the headmistress of Beauxbatons Academy of Magic','TRUE');
+INSERT INTO "TrueFalse" VALUES ('Xenophilius Lovegood is the author of The Life and Lies of Albus Dumbledore','FALSE');
+INSERT INTO "TrueFalse" VALUES ('R.A.B. is Sirius Black''s brother','TRUE');
+INSERT INTO "TrueFalse" VALUES ('The first name of Luna''s father is Augustus','FALSE');
+INSERT INTO "TrueFalse" VALUES ('Professor Slughorn calls Ron ''Wallenby'' after inviting Harry and Hermione to join him for his supper club','TRUE');
+INSERT INTO "TrueFalse" VALUES ('According to legend, a monster is inside the Chamber of Secrets','TRUE');
+INSERT INTO "TrueFalse" VALUES ('One of the Triwizard Tournament champions abandons the second task without completing it','TRUE');
+INSERT INTO "TrueFalse" VALUES ('The phrase ''A circle has no beginning'' appears on the Golden Snitch after Harry presses it against his lips','FALSE');
+INSERT INTO "TrueFalse" VALUES ('Sirius Black is related to a previous headmaster at Hogwarts, Phineas Niggelus','TRUE');
+INSERT INTO "TrueFalse" VALUES ('The sword of Gryffindor has the power to destroy Horcruxes because it imbibed Basilisk venom in the Chamber of Secrets','TRUE');
+INSERT INTO "ShortAnswer" VALUES ('Who creates the slug club?','Slughorn');
+INSERT INTO "ShortAnswer" VALUES ('What is the name of the group of students that practice Defence Against the Dark Arts under Harry''s guidance?','Dumbledore''s Army');
+INSERT INTO "ShortAnswer" VALUES ('Which Weasley twin has his ear cursed off by the Death Eaters when transporting Harrry to The Burrow?','George');
+INSERT INTO "ShortAnswer" VALUES ('What is the name of the driver of the Knight bus?','Ernie Prang');
+INSERT INTO "ShortAnswer" VALUES ('Who has the first name Dolores?','Umbridge');
+INSERT INTO "ShortAnswer" VALUES ('Luna''s father is editor of which newspaper?','The Quibbler');
+INSERT INTO "ShortAnswer" VALUES ('Who is the editor of the Daily Prophet?','Barnabus Cuffe');
+INSERT INTO "ShortAnswer" VALUES ('Who does Hermione impersonate using Polyjuice Potion when she, Ron, Harry, and Griphook break into Gringotts?','Bellatrix');
+INSERT INTO "ShortAnswer" VALUES ('What is Grindelwald''s first name?','Gellert');
+INSERT INTO "ShortAnswer" VALUES ('What is Mad-Eye Moody''s first name?','Alastor');
+INSERT INTO "ShortAnswer" VALUES ('How many times has Gilderoy Lockhart won Witch Weekly''s Most-Charming-Smile Award?','Five');
+INSERT INTO "ShortAnswer" VALUES ('What is Nicolas Flamel''s profession?','Alchemist');
+INSERT INTO "ShortAnswer" VALUES ('What are Dark-wizard-catchers known as?','Aurors');
+INSERT INTO "ShortAnswer" VALUES ('Whom was Snape in love with from childhood to the day of his death?','Lily');
+INSERT INTO "ShortAnswer" VALUES ('What is Krum''s first name?','Viktor');
+INSERT INTO "ShortAnswer" VALUES ('What is Gregorovitch''s profession?','Wandmaker');
+INSERT INTO "ShortAnswer" VALUES ('Who was Katie Bell convinced she had to deliver the cursed necklace to?','Dumbledore');
+INSERT INTO "ShortAnswer" VALUES ('What is the name of the landlord of the Leaky Cauldron?','Tom');
+INSERT INTO "ShortAnswer" VALUES ('Who is the Half-Blood Prince?','Snape');
+INSERT INTO "ShortAnswer" VALUES ('What is the only part of Peter Pettigrew''s body that was found after he was allegedly murdered?','Finger');
+INSERT INTO "ShortAnswer" VALUES ('What is Tonk''s first name, which she hates?','Nymphadora');
+INSERT INTO "ShortAnswer" VALUES ('How many brothers does Ron have?','Five');
+INSERT INTO "ShortAnswer" VALUES ('What is Lupin''s middle initial?','J');
+INSERT INTO "ShortAnswer" VALUES ('When Professor Trelawney predicts ''servant and master shall be reunited once more'', she is referring to which servant?','Peter Pettigrew');
+INSERT INTO "ShortAnswer" VALUES ('What is Filch''s first name?','Argus');
+INSERT INTO "ShortAnswer" VALUES ('For how many years has Sirius Black been in Azkaban prison?','Twelve');
+INSERT INTO "ShortAnswer" VALUES ('What is Luna Lovegood''s nickname given to her by her peers?','Loony');
+INSERT INTO "ShortAnswer" VALUES ('Who is Remus Lupin''s wife?','Tonks');
+INSERT INTO "ShortAnswer" VALUES ('Who erases herself from family pictures to protect her parents from the Dark Forces?','Hermione');
+INSERT INTO "ShortAnswer" VALUES ('Harry first apparates alongside whom?','Dumbledore');
+INSERT INTO "ShortAnswer" VALUES ('What relation are Albus and Aberforth Dumbledore?','Brothers');
+INSERT INTO "ShortAnswer" VALUES ('What is Professor Dumbledore''s first name?','Albus');
+INSERT INTO "ShortAnswer" VALUES ('What is the name of Hermione''s cat?','Crookshanks');
+INSERT INTO "ShortAnswer" VALUES ('Who frees Harry and his friends from the dungeons in Malfoy Manor?','Dobby');
+INSERT INTO "ShortAnswer" VALUES ('What colour handbag does the Snape Boggart carry?','Red');
+INSERT INTO "ShortAnswer" VALUES ('Which creature aids in the escape of Harry, Ron, and Hermione from Gringotts?','A dragon');
+INSERT INTO "ShortAnswer" VALUES ('What do Harry and Hermione use to travel back in time?','A Timeturner');
+INSERT INTO "ShortAnswer" VALUES ('What form does the Patronus take that leads Harry to the lake where he finds the sword of Gryffindor?','A doe');
+INSERT INTO "ShortAnswer" VALUES ('Who kills Sirius Black during the battle at the Ministry of Magic?','Bellatrix');
+INSERT INTO "ShortAnswer" VALUES ('Which Hogwarts student is harmed by a cursed necklace?','Katie Bell');
+INSERT INTO "ShortAnswer" VALUES ('Who does Harry hear singing when he opens the golden egg under water?','Merpeople');
+INSERT INTO "ShortAnswer" VALUES ('What does Dumbledore conjure to protect himself and Harry from the Inferi in the cave?','Fire');
+INSERT INTO "ShortAnswer" VALUES ('What kind of creature attacks Hermione in the girls'' toilets?','A troll');
+INSERT INTO "ShortAnswer" VALUES ('How many horcruxes did Lord Voldemort create?','Seven');
+INSERT INTO "ShortAnswer" VALUES ('Who became master of the Elder Wand after Dumbledore?','Draco');
+INSERT INTO "ShortAnswer" VALUES ('What magical creature do Harry and Draco find dead in the Forbidden Forest?','A unicorn');
+INSERT INTO "ShortAnswer" VALUES ('What should you never do to a Hippogriff, as they are very proud creatures?','Insult them');
+INSERT INTO "ShortAnswer" VALUES ('Which potion allows the drinker to temporarily assume the form of someone else?','Polyjuice Potion');
+INSERT INTO "ShortAnswer" VALUES ('What is Professor Slughorn''s favourite treat, as discovered by Tom Riddle?','Crystallized Pineapple');
+INSERT INTO "ShortAnswer" VALUES ('What is Horace Slughorn disguised as when Harry first meets him?','An armchair');
+INSERT INTO "ShortAnswer" VALUES ('What is Tom Riddle''s middle name?','Marvolo');
+INSERT INTO "ShortAnswer" VALUES ('Which professor stands up to Professor Snape before the Battle of Hogwarts?','McGonagall');
+INSERT INTO "ShortAnswer" VALUES ('Who binds the Unbreakable Vow between Professor Snape and Narcisssa Malfoy?','Bellatrix');
 INSERT INTO "MultipleChoice" VALUES ('Who kills Cedric Diggory on Lord Voldemort''s orders?','c','Draco Malfoy','Severus Snape','Peter Pettigrew','Sirius Black');
 INSERT INTO "MultipleChoice" VALUES ('Who instructs the Hogwarts suits of armour to defend the castle, using the spell Piertotum Locomotor, during the battle of Hogwarts?','d','Professor Trelawney','Alastor Moody','Professor Filius Flitwick','Professor McGonagall');
 INSERT INTO "MultipleChoice" VALUES ('Which one of the three shapes in the Deathly Hallows symbol represents the Invisibility Cloak?','a','Triangle','Circle','Line','Square');
@@ -45,23 +109,37 @@ INSERT INTO "MultipleChoice" VALUES ('Gwenog Jones is the captain of which Quidd
 INSERT INTO "MultipleChoice" VALUES ('Who is the Minister for Magic after Cornelius Fudge?','a','Rufus Scrimgeour','Pius Thicknesse','Millicent Bagnold','Ignatius Tuft');
 INSERT INTO "MultipleChoice" VALUES ('On whose grave does Hermione see the symbol the Xenophilius Lovegood was wearing on a necklace?','c','James and Lily Potter''s','Godric Gryffindor''s','Ignotus Peverell''s','Charity Burbage');
 INSERT INTO "MultipleChoice" VALUES ('What department in the Ministry of Magic does Arthur Weasley work in when Harry first meets him?','c','The Improper Use of Magic Office','The Muggle-Worthy Excuse Office','The Misuse of Muggle Artefacts Office','The Goblin Liaison Office');
-INSERT INTO "ShortAnswer" VALUES ('Question','Answer');
-INSERT INTO "ShortAnswer" VALUES ('Who creates the slug club?','Slughorn');
-INSERT INTO "ShortAnswer" VALUES ('What is the name of the group of students that practice Defence Against the Dark Arts under Harry''s guidance?','Dumbledore''s Army');
-INSERT INTO "ShortAnswer" VALUES ('Which Weasley twin has his ear cursed off by the Death Eaters when transporting Harrry to The Burrow?','George');
-INSERT INTO "ShortAnswer" VALUES ('What is the name of the driver of the Knight bus?','Ernie Prang');
-INSERT INTO "ShortAnswer" VALUES ('Who has the first name Dolores?','Umbridge');
-INSERT INTO "ShortAnswer" VALUES ('Luna''s father is editor of which newspaper?','The Quibbler');
-INSERT INTO "ShortAnswer" VALUES ('Who is the editor of the Daily Prophet?','Barnabus Cuffe');
-INSERT INTO "ShortAnswer" VALUES ('Who does Hermione impersonate using Polyjuice Potion when she, Ron, Harry, and Griphook break into Gringotts?','Bellatrix');
-INSERT INTO "ShortAnswer" VALUES ('What is Grindelwald''s first name?','Gellert');
-INSERT INTO "ShortAnswer" VALUES ('What is Mad-Eye Moody''s first name?','Alastor');
-INSERT INTO "ShortAnswer" VALUES ('How many times has Gilderoy Lockhart won Witch Weekly''s Most-Charming-Smile Award?','Five');
-INSERT INTO "ShortAnswer" VALUES ('What is Nicolas Flamel''s profession?','Alchemist');
-INSERT INTO "ShortAnswer" VALUES ('What are Dark-wizard-catchers known as?','Aurors');
-INSERT INTO "ShortAnswer" VALUES ('Whom was Snape in love with from childhood to the day of his death?','Lily');
-INSERT INTO "ShortAnswer" VALUES ('What is Krum''s first name?','Viktor');
-INSERT INTO "ShortAnswer" VALUES ('What is Gregorovitch''s profession?','Wandmaker');
-INSERT INTO "ShortAnswer" VALUES ('Who was Katie Bell convinced she had to deliver the cursed necklace to?','Dumbledore');
-INSERT INTO "ShortAnswer" VALUES ('What is the name of the landlord of the Leaky Cauldron?','Tom');
+INSERT INTO "MultipleChoice" VALUES ('Who saves Harry from drowning in the frozen lake in the Forest of Dean?','c','Neville','Luna','Ron','Hermione');
+INSERT INTO "MultipleChoice" VALUES ('What is Hermione''s middle name?','a','Jean','Ann','Grace','Rose');
+INSERT INTO "MultipleChoice" VALUES ('Which teams play in the 422nd Quidditch World Cup final?','d','Ireland and England','Scotland and Bulgaria','Bulgaria and England','Ireland and Bulgaria');
+INSERT INTO "MultipleChoice" VALUES ('What is the name of Cedric Diggory''s father?','b','Lionel','Amos','Edward','Tom');
+INSERT INTO "MultipleChoice" VALUES ('What color is the Beauxbatons school uniform?','a','Blue','Green','Pink','Violet');
+INSERT INTO "MultipleChoice" VALUES ('Who finds Harry and Dudley in the underpass after the Dementor attack?','c','Petunia','Dumbledore','Mrs. Figg','Vernon');
+INSERT INTO "MultipleChoice" VALUES ('Which Hogwarts student does Ariana Dumbledore bring to Harry, Ron, and Hermione through her portrait?','b','Ginny','Neville','Luna','Cho');
+INSERT INTO "MultipleChoice" VALUES ('What position does Krum play on the Bulgarian Quidditch Team?','d','Keeper','Beater','Chaser','Seeker');
+INSERT INTO "MultipleChoice" VALUES ('Which member of the Order of the Phoenix escorts Ron to The Burrow during the escape from Privet Drive?','b','Mad-Eye Moody','Tonks','Hagrid','Lupin');
+INSERT INTO "MultipleChoice" VALUES ('Which member of the Order of the Phoenix has scars running down his face because of an encounter with Fenrir Greyback, the werewolf?','c','Remus Lupin','Kingsley Shacklebolt','Bill Weasley','Mundugus Fletcher');
+INSERT INTO "MultipleChoice" VALUES ('According to Griphook, who put the fake sword of Gryffindor inside Bellatrix Lestrange''s vault?','b','Voldemort','Professor Snape','Bellatrix Lestrange','Regulus Black');
+INSERT INTO "MultipleChoice" VALUES ('Sybill Trelawney''s first prophecy speaks of a boy born at the end of which month?','d','August','January','October','July');
+INSERT INTO "MultipleChoice" VALUES ('What is the name of the Ministry of Magic employee that Ron impersonates using Polyjuice Potion?','c','Albert Runcorn','Pius Thicknesse','Reg Cattermole','Lugo Bagman');
+INSERT INTO "MultipleChoice" VALUES ('The initials R.A.B. belong to whom?','b','Regina Amelia Bones','Regulus Arcturus Black','Reginald Albert Black','Ryan Argus Binns');
+INSERT INTO "MultipleChoice" VALUES ('Who was forced to send his own son to Azkaban for being a Death Eater?','a','Barty Crouch','Alecto Carrow','Dedalus Diggle','Cornelius Fudge');
+INSERT INTO "MultipleChoice" VALUES ('What is the name of Gilderoy Lockhart''s self-praising autobiography?','d','Who Am I?','Charm & Charms','Gilderoy Lockhart','Magical Me');
+INSERT INTO "MultipleChoice" VALUES ('What is Madame Maxime''s first name?','c','Augusta','Mafalda','Olympe','Aurora');
+INSERT INTO "MultipleChoice" VALUES ('Who does Tonks marry?','b','Bill Weasley','Remus Lupin','Charlie Weasley','Florean Fortescue');
+INSERT INTO "MultipleChoice" VALUES ('Which of the three brothers from The Tale of the Three Brothers presents himself to Death during old age?','a','The youngest brother','The second brother','The eldest brother','None of the brothers');
+INSERT INTO "MultipleChoice" VALUES ('Which of Ron''s brothers marries Fleur Delacour?','d','Charlie','Percy','Fred','Bill');
+INSERT INTO "MultipleChoice" VALUES ('After Dumbledore''s death, who writes an unofficial autobiography on him?','b','Bathilda Bagshot','Rita Skeeter','Gilderoy Lockhart','Xenophilius Lovegood');
+INSERT INTO "MultipleChoice" VALUES ('What is Ron Weasley''s middle name?','c','Oliver','Peter','Bilius','Frank');
+INSERT INTO "MultipleChoice" VALUES ('What is the name of the Slytherin Quidditch captain during Harry''s second year at Hogwarts?','b','Blaise Zabini','Marcus Flint','Draco Malfoy','Theodore Nott');
+INSERT INTO "MultipleChoice" VALUES ('Where does Charlie Weasley live when he is studying dragons?','c','Spain','Norway','Romania','Egypt');
+INSERT INTO "MultipleChoice" VALUES ('Who wears the symbol of the Deathly Hallows around their neck at the wedding?','d','Emmeline Vance','Andromeda Tonks','Stan Shunpike','Xenophilius Lovegood');
+INSERT INTO "MultipleChoice" VALUES ('Who founded the Order of the Phoenix when the fight against Lord Voldemort began?','b','Kingsley Shacklebolt','Dumbledore','Caradoc Dearborn','Benjy Fenwick');
+INSERT INTO "MultipleChoice" VALUES ('Which former Hogwarts professor is the Head of the Muggle-born Registration Commission?','b','Horace Slughorn','Dolores Umbridge','Arthur Weasley','Elphias Doge');
+INSERT INTO "MultipleChoice" VALUES ('What are the names of Neville Longbottom''s parents?','d','Anthony and Marge','Ludo and Amelia','Silvanus and Olympe','Frank and Alice');
+INSERT INTO "MultipleChoice" VALUES ('What is the full name of the conductor on the Knight Bus?','b','Antonin Dolohov','Stan Shunpike','Florean Fortescue','Dirk Cresswell');
+INSERT INTO "MultipleChoice" VALUES ('Who do Harry and Hermione run into in an elevator at the Ministry of Magic?','a','Dolores Umbridge','Arthur Weasley','Amos Diggory','Walden Macnair');
+INSERT INTO "MultipleChoice" VALUES ('Why can only some people see Thestrals?','c','They choose to be seen by people they trust','No one knows','They can only be seen by people who have seen death','They can only be seen by half-bloods');
+INSERT INTO "MultipleChoice" VALUES ('What spell does Hermione use to stop the rogue Bludger that targets Harry during a Quidditch match against Slytherin?','b','Accio','Finite Incantatem','Confundo','Protego');
+INSERT INTO "MultipleChoice" VALUES ('What type of dragon does Cedric face in the first task of the Triwizard Tournament?','a','The Swedish Short-Snout','The Romanian Longhorn','The Hebridean Black','The Hungarian Horntail');
 COMMIT;
